a59f66b30a4949e4bfedcabe83724eeab92582ff
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-01-24 21:31:51 +0800

    fixed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-
 $(function() {
     $("#add").on("click", function() {
         var jem = $("input").val();
         if(jem !== '') {
             var elem = $("<li></li>").text(jem);
-            $(elem).append("<button class='rem'>Clear</button>"); //add class rem
+            $(elem).append("<button class='rem'>X</button>");
             $("#my-list").append(elem);
             $("input").val("");
-            $(".rem").on("click", function() {
-                $(this).parent().remove();
-            }); 
         }
+    });
+
+    $(document).on("click", ".rem", function() {
+        $(this).parent().remove();
     });
 });
